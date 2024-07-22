f744a7ee042c485a13bb4391275d5facd8482a99
Jordan <jordanharris1833@yahoo.com>
2024-07-22 11:59:48 -0500

    added files and finished workshop

---
+++ vault.js
@@ -0,0 +1,14 @@
+// writing the users secret message
+const message =
+  "You have recieved this message because you have been chosen to open an important vault. The combination is:";
+
+// assigning combination variables
+const com1 = 12 - 2;
+const com2 = 25 + 15;
+const com3 = 50 - 11;
+
+// calling all variables for final dialog box
+const final = `${message} ${com1} - ${com2} - ${com3}`;
+
+// creating dialog box
+alert(final);
